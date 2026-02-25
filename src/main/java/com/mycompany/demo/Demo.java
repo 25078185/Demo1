2ce19dfadcc3d1657b92b34218e869c188e69b60
Lwando Matiwane <lwandomatiwane84@gmail.com>
2026-02-25 21:23:44 +0200

    first commit

---
+++ src/main/java/com/mycompany/demo/Demo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.demo;
+
+/**
+ *
+ * @author Lwando
+ */
+public class Demo{
+    public static void main (String[] args){
+        System.out.println("HELLO WORLD");
+    }
+}
